63752d668b96228b4ef68854c76ad21595af969e
Koutilya Namdeo <koutilyanamdeo888@gmail.com>
2021-10-21 17:47:16 +0530

    first commit

---
+++ src/Alert/Alert.js
@@ -0,0 +1,9 @@
+function Alert () {
+    ['primary'].map((variant, idx) => (
+        <Alert key={idx} variant={variant}>
+          This is a {variant} alert—check it out!
+        </Alert>
+      ));
+    
+}
+export default Alert;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { Alert, Navbar } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Navbar bg="light">
+        <Container>
+          <Navbar.Brand>Student DashBoard</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <br />
+
+      <div className="marks">
+        <div className="disp-marks">
+          <p>Minimum Marks</p>
+          <p>Maximum Marks</p>
+          <p>Average Marks</p>
+        </div>
+
+        <br />
+
+        <div className="add-student">
+          <p>Add New Student</p>
+          <input
+            type="text"
+            name="student-name"
+            placeholder="Student Name"
+          ></input>
+          <input
+            type="number"
+            name="student-marks"
+            placeholder="Student Marks"
+          ></input>
+          <button className="register-btn">Register</button>
+        </div>
+        <br />
+
+        <div className="information-container">
+          <table className="tb">
+            <thead>
+              <tr className="table-heading">
+                <th>Roll Number</th>
+                <th>Student Name</th>
+                <th>Marks</th>
+                <th>Operation</th>
+              </tr>
+            </thead>
+            <tbody>
+              <Alert />
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
